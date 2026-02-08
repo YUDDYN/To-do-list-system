6d3e47387d09c9f8f7e3e6cd97e5f083b35ee6b5
YUDDYN <snowy.frost.fox@gmail.com>
2026-02-08 09:47:13 +0700

    Fix missing newline at end of file in main.js

--- To do list/main.js
+++ To do list/main.js
@@ -2,3 +2,16 @@ let tasks = [];
 document.addEventListener('DOMContentLoaded', () => {
     loadTasks();
 });
+
+function loadTasks() {
+    fetch('ambil_tugas.php')
+        .then(response => response.json())
+        .then(data => {
+            tasks = data.map(task => ({
+                ...task,
+                 schedule: new Date(task.jadwal),
+                createdAt: new Date(task.created_at)
+            }));
+            renderTasks();
+        });
+}
